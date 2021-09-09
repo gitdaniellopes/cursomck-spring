de61c211b031adcd9fc98a52c5aebfa930fde3df
Daniel <danieljfsantos@gmail.com>
2021-09-09 20:00:10 -0300

    Banco de dados H2 e criacao automatica da base de dados

--- src/main/kotlin/br/daniel/cursomck/models/Category.kt
+++ src/main/kotlin/br/daniel/cursomck/models/Category.kt
@@ -1,6 +1,15 @@
 package br.daniel.cursomck.models
 
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
 data class Category(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Int? = null,
     var name: String?
 )
