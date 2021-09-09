97594e2140d5c223081a221343a9f5d0aefa1e3e
Daniel <danieljfsantos@gmail.com>
2021-09-09 19:49:17 -0300

    Testando a primeira classe de dominio - Categoria

--- src/main/kotlin/br/daniel/cursomck/controller/CategoryController.kt
+++ src/main/kotlin/br/daniel/cursomck/controller/CategoryController.kt
@@ -1,5 +1,6 @@
 package br.daniel.cursomck.controller
 
+import br.daniel.cursomck.models.Category
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -9,7 +10,15 @@ import org.springframework.web.bind.annotation.RestController
 class CategoryController {
 
     @GetMapping
-    fun list(): String {
-        return "Rest ok"
+    fun list(): List<Category> {
+
+        val cat = Category(1, "Informatica")
+        val cat2 = Category(2, "Escritorio")
+
+        val list = mutableListOf<Category>()
+        list.add(cat)
+        list.add(cat2)
+
+        return list
     }
 }
---
+++ src/main/kotlin/br/daniel/cursomck/models/Category.kt
@@ -0,0 +1,6 @@
+package br.daniel.cursomck.models
+
+data class Category(
+    var id: Int? = null,
+    var name: String?
+)
