19ecf38b4407bc281ea06a387494df2d48737643
Daniel <danieljfsantos@gmail.com>
2021-09-09 19:28:12 -0300

    projeto criado

---
+++ src/main/kotlin/br/daniel/cursomck/CursomckApplication.kt
@@ -0,0 +1,11 @@
+package br.daniel.cursomck
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CursomckApplication
+
+fun main(args: Array<String>) {
+    runApplication<CursomckApplication>(*args)
+}
---
+++ src/test/kotlin/br/daniel/cursomck/CursomckApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.daniel.cursomck
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CursomckApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
